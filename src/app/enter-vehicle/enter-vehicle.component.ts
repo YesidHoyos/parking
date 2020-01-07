c7a7f6aa3d5259735fe1d41986166d9934521fda
Yesid Hoyos <yesidfernandohoyos@gmail.com>
2020-01-07 12:35:45 -0500

    the enter-vehicule component is created and added routing

---
+++ src/app/enter-vehicle/enter-vehicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-vehicle',
+  templateUrl: './enter-vehicle.component.html',
+  styleUrls: ['./enter-vehicle.component.css']
+})
+export class EnterVehicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
